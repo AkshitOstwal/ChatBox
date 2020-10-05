eadb0580fe052782ff1f141b9ceb5e9d4d1be879
Akshit Ostwal <akshitostwal@gmail.com>
2020-10-05 10:24:14 +0530

    Update SimpleChatClient.java

--- src/chatbox/SimpleChatClient.java
+++ src/chatbox/SimpleChatClient.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chatbox;
 import java.io.*;
 import java.net.*;
